8795912b38d2d3c9c509e764028734142ff84d09
Nikos Mavrogiannopoulos <nmav@redhat.com>
2015-08-27 17:17:56 +0200

    tests: integrated failover tests to the suite

--- tests/failover-tests.sh
+++ tests/failover-tests.sh
@@ -11,17 +11,21 @@ echo "This test will use a radius server on localhost"
 echo "and which can be executed with run-server.sh   "
 echo "***********************************************"
 
-TMPFILE=tmp.out
+TMPFILE=tmp-failover.out
 
 if test -z "$SERVER_IP";then
 	echo "the variable SERVER_IP is not defined"
 	exit 77
 fi
 
-sed 's/localhost/127.1.1.1:9999,'$SERVER_IP'/g' <$srcdir/radiusclient.conf >radiusclient-temp.conf
-sed 's/localhost/'$SERVER_IP'/g' <$srcdir/servers >servers-temp
+#socat UDP4-RECVFROM:9999 UDP4-SENDTO:$SERVER_IP:1812 &
+#SOPID=$!
 
-../src/radiusclient -D -i -f radiusclient-temp.conf  User-Name=test Password=test | tee $TMPFILE
+PID=$$
+sed -e 's/localhost/127.1.1.1:9999,'$SERVER_IP'/g' -e 's/servers-temp/servers-temp'$PID'/g' <$srcdir/radiusclient.conf >radiusclient-temp$PID.conf
+sed 's/localhost/'$SERVER_IP'/g' <$srcdir/servers >servers-temp$PID
+
+../src/radiusclient -D -i -f radiusclient-temp$PID.conf  User-Name=test Password=test | tee $TMPFILE
 if test $? != 0;then
 	echo "Error in PAP auth"
 	exit 1
@@ -62,9 +66,9 @@ if test $? != 0;then
 	exit 1
 fi
 
-rm -f servers-temp
+rm -f servers-temp$PID
 #cat $TMPFILE
 rm -f $TMPFILE
-rm -f radiusclient-temp.conf
+rm -f radiusclient-temp$PID.conf
 
 exit 0
