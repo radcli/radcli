d7e22a355fd6ba14a3eae7cb90e7b58b180717a0
Nikos Mavrogiannopoulos <nmav@redhat.com>
2015-11-26 18:12:07 +0100

    tests: cleanups in tls-tests.sh

--- tests/tls-tests.sh
+++ tests/tls-tests.sh
@@ -1,11 +1,35 @@
 #!/bin/sh
 
 # Copyright (C) 2014 Nikos Mavrogiannopoulos
+# Copyright (C) 2015 Red Hat, Inc
 #
-# License: BSD
+#   All rights reserved.
+#   
+#   Redistribution and use in source and binary forms, with or without
+#   modification, are permitted provided that the following conditions
+#   are met:
+#   1. Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#   
+#   THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+#   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#   ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+#   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+#   OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+#   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#   OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+#   SUCH DAMAGE.
 
 srcdir="${srcdir:-.}"
 TMPFILE=tmp$$.out
+CONFFILE=conf.tmp$$.out
+SERVERSFILE=servers.tmp$$.out
 
 echo "***********************************************"
 echo "This test will use a radius-tls server on localhost"
@@ -19,11 +43,11 @@ if test -z "$SERVER_IP";then
 fi
 
 PID=$$
-sed -e 's/localhost/'$SERVER_IP'/g' -e 's/servers-tls-temp/servers-tls-temp'$PID'/g' <$srcdir/dtls/radiusclient-tls.conf >radiusclient-tls-temp$PID.conf
-sed 's/localhost/'$SERVER_IP'/g' <$srcdir/servers >servers-tls-temp$PID
+sed -e 's/localhost/'$SERVER_IP'/g' -e 's/servers-tls-temp/servers-tls-temp'$PID'/g' <$srcdir/dtls/radiusclient-tls.conf >$CONFFILE
+sed 's/localhost/'$SERVER_IP'/g' <$srcdir/servers >$SERVERSFILE
 
 # Test whether a TLS session will succeed
-../src/radiusclient -f radiusclient-tls-temp$PID.conf  User-Name=test Password=test >$TMPFILE
+../src/radiusclient -D -f $CONFFILE  User-Name=test Password=test >$TMPFILE
 if test $? != 0;then
 	echo "Error in PAP auth"
 	exit 1
@@ -51,12 +75,12 @@ if test $? != 0;then
 fi
 
 # Test whether a TLS invalidated session for some reason will reconnect
-./tls-restart -f radiusclient-tls-temp$PID.conf  User-Name=test Password=test >$TMPFILE
+./tls-restart -f $CONFFILE  User-Name=test Password=test >$TMPFILE
 if test $? != 0;then
 	echo "Error in session restart"
 	exit 1
 fi
 
 rm -f $TMPFILE
-rm -f servers-tls-temp$PID radiusclient-tls-temp$PID.conf
+rm -f $SERVERSFILE $CONFFILE
 exit 0
