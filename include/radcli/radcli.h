e5780f8569d8508c9263f2ef6ad563d7910d29b9
Igor Mineev <mineev@cryptopro.ru>
2019-05-14 11:12:49 +0100

    Add functions to create dict without file reading.
    Co-Authored-By: amdei <amdei@users.noreply.github.com>

--- include/radcli/radcli.h
+++ include/radcli/radcli.h
@@ -112,7 +112,8 @@ typedef enum rc_attr_type {
 	PW_TYPE_IPADDR=2,	//!< The attribute is an IPv4 address in host-byte order.
 	PW_TYPE_DATE=3,		//!< The attribute contains a 32-bit number indicating the seconds since epoch.
 	PW_TYPE_IPV6ADDR=4,	//!< The attribute is an 128-bit IPv6 address.
-	PW_TYPE_IPV6PREFIX=5    //!< The attribute is an IPv6 prefix; the lvalue will indicate its size.
+	PW_TYPE_IPV6PREFIX=5,   //!< The attribute is an IPv6 prefix; the lvalue will indicate its size.
+	PW_TYPE_MAX=6   	//!< Maximum number of types (last+1)
 } rc_attr_type;
 
 /** \enum rc_standard_codes Standard RADIUS request codes
@@ -625,6 +626,11 @@ rc_socket_type rc_get_socket_type(rc_handle * rh);
 
 int rc_read_dictionary (rc_handle *rh, char const *filename);
 int rc_read_dictionary_from_buffer (rc_handle *rh, char const *buf, size_t size);
+
+DICT_ATTR *rc_dict_addattr(rc_handle *rh, char const * namestr, uint32_t value, int type, uint32_t vendorspec);
+DICT_VALUE *rc_dict_addval(rc_handle *rh, char const * attrstr, char const * namestr, uint32_t value);
+DICT_VENDOR *rc_dict_addvend(rc_handle *rh, char const * vendorname, uint32_t value);
+
 DICT_ATTR *rc_dict_getattr(rc_handle const *rh, uint64_t attribute);
 DICT_ATTR *rc_dict_findattr(rc_handle const *rh, char const *attrname);
 DICT_VALUE *rc_dict_findval(rc_handle const *rh, char const *valname);
