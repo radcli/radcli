6c0269bf7dabdf4bd0e9777f07c131731a9236b9
Nikos Mavrogiannopoulos <n.mavrogiannopoulos@gmail.com>
2026-04-06 11:03:10 +0200

    auto-generated web-pages
    Signed-off-by: Nikos Mavrogiannopoulos <n.mavrogiannopoulos@gmail.com>

---
+++ manual/search/all_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['main_20api_0',['Main API',['../group__radcli-api.html',1,'']]],
+  ['mgmt_5fpoll_5fsecret_1',['MGMT_POLL_SECRET',['../group__radcli-api.html#ga7e44bb92fd9feaa0d52bd5487c9faf21',1,'radcli.h']]],
+  ['miscellaneous_20api_2',['Miscellaneous API',['../group__misc-api.html',1,'']]]
+];
